8c6ef7acbc8d883a865bd37795caa9d5cf6b4ce9
Kristoffer Magnusson <1629860+rpsychologist@users.noreply.github.com>
2022-09-14 12:17:38 +0200

    refactor how s3 files are imported
    keep a list of imported files in
    `imported_s3.txt` and import only new
    files straight from OneDrive
    
    copy `.env.example` to `.env` and update
    `ONEDRIVE_PATH_S3`

--- import_s3.sh
+++ import_s3.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
-
-files=$(rsync --dry-run --recursive --checksum --verbose data-raw/playfab-s3/ data-raw/imported/playfab-s3/ | awk '/.json.gz/ {print "data-raw/playfab-s3/"$1}')
+source .env
+echo "OneDrive S3 Path:" $ONEDRIVE_PATH_S3
+all=$(find "$ONEDRIVE_PATH_S3" -name '*.json.gz' | awk -F "/playfab-s3/" '{print $2}' | sort)
+imported=$(cat data-raw/imported_s3.txt)
+# Import only new files
+files=$(diff <(echo "$imported") <(echo "$all") | awk -F "> " '/.json.gz/ {print $2}')
 for file in $files; do 
-    echo "$file"
-    gunzip -c $file | jq -c '.EventData' | docker exec -i pws_postgres /usr/bin/psql -U postgres -c 'COPY tmp (data) FROM STDIN'
+    echo "Importing: $file"
+    gunzip -c "${ONEDRIVE_PATH_S3}${file}" | jq -c '.EventData' | docker exec -i pws_postgres /usr/bin/psql -U postgres -c 'COPY tmp (data) FROM STDIN'
     docker exec -i pws_postgres /usr/bin/psql -U postgres --set=pwstable='pws_s3' --set=devicetable='pws_device_info_s3' -f sql/import.sql
+done
+echo "Do you whish to update 'imported_s3.txt'?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) mv data-raw/imported_s3.txt data-raw/imported_s3.txt.bak;
+            echo "$all" > data-raw/imported_s3.txt; 
+            break;;
+        No ) exit;;
+    esac
 done
